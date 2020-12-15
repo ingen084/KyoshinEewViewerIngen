7d3abe3220fc3c66c1ab376639b415a6a8aa2820
ingen084 <ingen188@gmail.com>
2020-12-16 00:52:41 +0900

    クラッシュする問題を修正

--- src/KyoshinEewViewer/Services/JmaXmlPullReceiveService.cs
+++ src/KyoshinEewViewer/Services/JmaXmlPullReceiveService.cs
@@ -38,7 +38,7 @@ namespace KyoshinEewViewer.Services
 		{
 			AutomaticDecompression = DecompressionMethods.All
 		});
-		private List<Guid> ParsedMessages { get; } = new List<Guid>();
+		private List<string> ParsedMessages { get; } = new List<string>();
 
 		private DateTime LastElapsedTime { get; set; } = DateTime.MinValue;
 		private DateTime LastChecked { get; set; } = DateTime.MinValue;
@@ -121,8 +121,7 @@ namespace KyoshinEewViewer.Services
 			// URLにないものを抽出
 			foreach (var item in matchItems)
 			{
-				var guid = new Guid(new Uri(new Uri(item.Id).LocalPath).LocalPath);
-				if (ParsedMessages.Contains(guid))
+				if (ParsedMessages.Contains(item.Id))
 					continue;
 
 				Logger.Trace($"処理 {item.LastUpdatedTime:yyyy/MM/dd HH:mm:ss} {item.Title.Text}");
@@ -229,7 +228,7 @@ namespace KyoshinEewViewer.Services
 					{
 						Logger.Error("デシリアライズ時に例外が発生しました。 " + ex);
 					}
-					ParsedMessages.Add(guid);
+					ParsedMessages.Add(item.Id);
 					break;
 				}
 
