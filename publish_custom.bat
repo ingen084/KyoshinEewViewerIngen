29119c09784990acf50e079485dabc306737a9d7
ingen084 <ingen188@gmail.com>
2021-05-10 11:40:01 +0900

    RtRをいったん一括で無効に

--- publish_custom.bat
+++ publish_custom.bat
@@ -1,4 +1,4 @@
-dotnet publish src/KyoshinEewViewer/KyoshinEewViewer.csproj -r %1 -c release -o tmp/%1_%2 -p:PublishReadyToRun=%3 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained %3
+dotnet publish src/KyoshinEewViewer/KyoshinEewViewer.csproj -r %1 -c release -o tmp/%1_%2 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained %3
 del tmp\%1_%2\*.pdb
 
 powershell -c "Compress-Archive -Path tmp/%1_%2/* -DestinationPath tmp/KyoshinEewViewer_ingen_%1_%2.zip"
