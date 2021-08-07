07816d0738446ea974e089d469f9fabf202a2534
ingen084 <ingen188@gmail.com>
2021-08-08 03:56:54 +0900

    左右で途切れないように

--- src/KyoshinEewViewer.Map/Layers/LandLayer.cs
+++ src/KyoshinEewViewer.Map/Layers/LandLayer.cs
@@ -75,11 +75,11 @@ namespace KyoshinEewViewer.Map.Layers
 			Style = SKPaintStyle.Fill,
 			Color = new SKColor(242, 239, 233),
 		};
-		private SKPaint SpecialLandFill { get; set; } = new SKPaint
-		{
-			Style = SKPaintStyle.Fill,
-			Color = new SKColor(242, 239, 233),
-		};
+		//private SKPaint SpecialLandFill { get; set; } = new SKPaint
+		//{
+		//	Style = SKPaintStyle.Fill,
+		//	Color = new SKColor(242, 239, 233),
+		//};
 		private SKPaint OverSeasLandFill { get; set; } = new SKPaint
 		{
 			Style = SKPaintStyle.Fill,
@@ -131,14 +131,14 @@ namespace KyoshinEewViewer.Map.Layers
 				IsAntialias = false,
 			};
 
-			SpecialLandFill = new SKPaint
-			{
-				Style = SKPaintStyle.Stroke,
-				Color = SKColors.Red,
-				MaskFilter = SKMaskFilter.CreateBlur(SKBlurStyle.Normal, 3),
-				StrokeWidth = 5,
-				IsAntialias = true,
-			};
+			//SpecialLandFill = new SKPaint
+			//{
+			//	Style = SKPaintStyle.Stroke,
+			//	Color = SKColors.Red,
+			//	MaskFilter = SKMaskFilter.CreateBlur(SKBlurStyle.Normal, 3),
+			//	StrokeWidth = 5,
+			//	IsAntialias = true,
+			//};
 
 			OverSeasLandFill = new SKPaint
 			{
@@ -162,15 +162,12 @@ namespace KyoshinEewViewer.Map.Layers
 				var baseZoom = (int)Math.Ceiling(Zoom);
 				// 実際のズームに合わせるためのスケール
 				var scale = Math.Pow(2, Zoom - baseZoom);
-
-				var leftTop = LeftTopLocation.CastLocation().ToPixel(Projection, baseZoom);
-
 				canvas.Scale((float)scale);
+				// 画面座標への変換
+				var leftTop = LeftTopLocation.CastLocation().ToPixel(Projection, baseZoom);
 				canvas.Translate((float)-leftTop.X, (float)-leftTop.Y);
 
-				// とりあえず海外の描画を行う
-				RenderOverseas(canvas, baseZoom);
-
+				// 使用するレイヤー決定
 				var useLayerType = LandLayerType.EarthquakeInformationSubdivisionArea;
 				//if (baseZoom > 6)
 				//	useLayerType = LandLayerType.PrefectureForecastArea;
@@ -179,67 +176,96 @@ namespace KyoshinEewViewer.Map.Layers
 				if (baseZoom > 10)
 					useLayerType = LandLayerType.MunicipalityEarthquakeTsunamiArea;
 
+				// スケールに合わせてブラシのサイズ変更
 				CoastlineStroke.StrokeWidth = (float)(CoastlineStrokeWidth / scale);
 				PrefStroke.StrokeWidth = (float)(PrefStrokeWidth / scale);
 				AreaStroke.StrokeWidth = (float)(AreaStrokeWidth / scale);
-				SpecialLandFill.StrokeWidth = (float)(Zoom / scale);
-				SpecialLandFill.MaskFilter = SKMaskFilter.CreateBlur(SKBlurStyle.Normal, (float)(3f / scale));
+				//SpecialLandFill.StrokeWidth = (float)(Zoom / scale);
+				//SpecialLandFill.MaskFilter = SKMaskFilter.CreateBlur(SKBlurStyle.Normal, (float)(3f / scale));
 
 				if (!Controllers.TryGetValue(useLayerType, out var layer))
 					return;
-				foreach (var f in layer.FindPolygon(ViewAreaRect))
+
+				RenderRect(ViewAreaRect);
+				// 左右に途切れないように補完して描画させる
+				if (ViewAreaRect.Bottom > 180)
 				{
-					if (CustomColorMap != null &&
-						CustomColorMap.TryGetValue(useLayerType, out var map) &&
-						map.TryGetValue(f.Code ?? -1, out var color))
-					{
-						var oc = LandFill.Color;
-						LandFill.Color = color;
-						f.Draw(canvas, Projection, baseZoom, LandFill);
-						LandFill.Color = oc;
-					}
-					else
-						f.Draw(canvas, Projection, baseZoom, LandFill);
+					canvas.Translate((float)new KyoshinMonitorLib.Location(0, 180).ToPixel(Projection, baseZoom).X, 0);
 
-					//if (f.Code == 270000)
-					//{
-					//	var path = f.GetOrCreatePath(Projection, baseZoom);
+					var fixedRect = ViewAreaRect;
+					fixedRect.Y -= 360;
 
-					//	canvas.Save();
-					//	canvas.ClipPath(path);
-					//	canvas.DrawPath(path, SpecialLandFill);
-					//	canvas.Restore();
-					//}
+					RenderRect(fixedRect);
+				}
+				else if (ViewAreaRect.Top < -180)
+				{
+					canvas.Translate((float)new KyoshinMonitorLib.Location(0, -180).ToPixel(Projection, baseZoom).X, 0);
+
+					var fixedRect = ViewAreaRect;
+					fixedRect.Y += 360;
+
+					RenderRect(fixedRect);
 				}
 
-				if (CustomColorMap != null)
-					foreach (var cLayerType in CustomColorMap.Keys)
-						if (cLayerType != useLayerType && Controllers.TryGetValue(cLayerType, out var clayer))
-							foreach (var f in clayer.FindPolygon(ViewAreaRect))
-								if (CustomColorMap[cLayerType].TryGetValue(f.Code ?? -1, out var color))
-								{
-									var oc = LandFill.Color;
-									LandFill.Color = color;
-									f.Draw(canvas, Projection, baseZoom, LandFill);
-									LandFill.Color = oc;
-								}
-
-				foreach (var f in layer.FindLine(ViewAreaRect))
+				void RenderRect(RectD subViewArea)
 				{
-					switch (f.Type)
+					// とりあえず海外の描画を行う
+					RenderOverseas(canvas, baseZoom, subViewArea);
+
+					foreach (var f in layer.FindPolygon(subViewArea))
 					{
-						case FeatureType.AdminBoundary:
-							if (!InvalidatePrefStroke && baseZoom > 4.5)
-								f.Draw(canvas, Projection, baseZoom, PrefStroke);
-							break;
-						case FeatureType.Coastline:
-							if (!InvalidateLandStroke && baseZoom > 4.5)
-								f.Draw(canvas, Projection, baseZoom, CoastlineStroke);
-							break;
-						case FeatureType.AreaBoundary:
-							if (!InvalidateAreaStroke && baseZoom > 4.5)
-								f.Draw(canvas, Projection, baseZoom, AreaStroke);
-							break;
+						if (CustomColorMap != null &&
+							CustomColorMap.TryGetValue(useLayerType, out var map) &&
+							map.TryGetValue(f.Code ?? -1, out var color))
+						{
+							var oc = LandFill.Color;
+							LandFill.Color = color;
+							f.Draw(canvas, Projection, baseZoom, LandFill);
+							LandFill.Color = oc;
+						}
+						else
+							f.Draw(canvas, Projection, baseZoom, LandFill);
+
+						//if (f.Code == 270000)
+						//{
+						//	var path = f.GetOrCreatePath(Projection, baseZoom);
+
+						//	canvas.Save();
+						//	canvas.ClipPath(path);
+						//	canvas.DrawPath(path, SpecialLandFill);
+						//	canvas.Restore();
+						//}
+					}
+
+					if (CustomColorMap != null)
+						foreach (var cLayerType in CustomColorMap.Keys)
+							if (cLayerType != useLayerType && Controllers.TryGetValue(cLayerType, out var clayer))
+								foreach (var f in clayer.FindPolygon(subViewArea))
+									if (CustomColorMap[cLayerType].TryGetValue(f.Code ?? -1, out var color))
+									{
+										var oc = LandFill.Color;
+										LandFill.Color = color;
+										f.Draw(canvas, Projection, baseZoom, LandFill);
+										LandFill.Color = oc;
+									}
+
+					foreach (var f in layer.FindLine(subViewArea))
+					{
+						switch (f.Type)
+						{
+							case FeatureType.AdminBoundary:
+								if (!InvalidatePrefStroke && baseZoom > 4.5)
+									f.Draw(canvas, Projection, baseZoom, PrefStroke);
+								break;
+							case FeatureType.Coastline:
+								if (!InvalidateLandStroke && baseZoom > 4.5)
+									f.Draw(canvas, Projection, baseZoom, CoastlineStroke);
+								break;
+							case FeatureType.AreaBoundary:
+								if (!InvalidateAreaStroke && baseZoom > 4.5)
+									f.Draw(canvas, Projection, baseZoom, AreaStroke);
+								break;
+						}
 					}
 				}
 			}
@@ -251,12 +277,12 @@ namespace KyoshinEewViewer.Map.Layers
 		/// <summary>
 		/// 海外を描画する
 		/// </summary>
-		private void RenderOverseas(SKCanvas canvas, int baseZoom)
+		private void RenderOverseas(SKCanvas canvas, int baseZoom, RectD subViewArea)
 		{
 			if (!Controllers.ContainsKey(LandLayerType.WorldWithoutJapan))
 				return;
 
-			foreach (var f in Controllers[LandLayerType.WorldWithoutJapan].FindPolygon(ViewAreaRect))
+			foreach (var f in Controllers[LandLayerType.WorldWithoutJapan].FindPolygon(subViewArea))
 				f.Draw(canvas, Projection, baseZoom, OverSeasLandFill);
 		}
 	}
