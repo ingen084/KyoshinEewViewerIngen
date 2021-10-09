de987e4aa313a6d1afa2e80c15cbce484d549e2e
ingen084 <ingen188@gmail.com>
2021-10-10 01:29:17 +0900

    ディレクトリ修正

--- publish_osx.sh
+++ publish_osx.sh
@@ -2,7 +2,7 @@
 
 ~/.dotnet/dotnet restore -r osx-x64 src/KyoshinEewViewer/KyoshinEewViewer.csproj
 ~/.dotnet/dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=$2  -p:Configuration=Release src/KyoshinEewViewer/KyoshinEewViewer.csproj -p:PublishDir=../../tmp/$2_$3 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:UseAppHost=true
-rm "tmp/$2_$3/KyoshinEewViewer"
-rm "tmp/$2_$3/*.dylib"
-chmod +x tmp/$2_$3/KyoshinEewViewer
+rm tmp/$2_$3/KyoshinEewViewer
+rm tmp/$2_$3/*.dylib
+chmod +x tmp/$2_$3/KyoshinEewViewer.app/Contents/MacOS/KyoshinEewViewer
 cd tmp/$2_$3; zip -r ../KyoshinEewViewer_ingen_$2_$3.zip *
