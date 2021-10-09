b787a035e25663bf03caefe4afc5642951fdab7c
ingen084 <ingen188@gmail.com>
2021-10-09 23:29:02 +0900

    コマンド修正

--- publish_osx.bat
+++ publish_osx.bat
@@ -1,6 +1,6 @@
 dotnet restore -r osx-x64 src/KyoshinEewViewer/KyoshinEewViewer.csproj
 dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=%2  -p:Configuration=Release src/KyoshinEewViewer/KyoshinEewViewer.csproj -p:PublishDir=../../tmp/%2_%3 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:UseAppHost=true
-rm tmp/%2_%3/KyoshinEewViewer
-rm tmp/%2_%3/*.dylib
+del tmp/%2_%3/KyoshinEewViewer
+del tmp/%2_%3/*.dylib
 
 powershell -c "Compress-Archive -Path tmp/%2_%3/* -DestinationPath tmp/KyoshinEewViewer_ingen_%2_%3.zip"
