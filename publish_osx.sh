e10404e865de2379b5f9a2b7a8accd1776473e3b
ingen084 <ingen188@gmail.com>
2022-01-03 02:08:54 +0900

    圧縮対象を変更

--- publish_osx.sh
+++ publish_osx.sh
@@ -2,7 +2,5 @@
 
 ~/.dotnet/dotnet restore -r $2 src/KyoshinEewViewer/KyoshinEewViewer.csproj
 ~/.dotnet/dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=$2  -p:Configuration=Release src/KyoshinEewViewer/KyoshinEewViewer.csproj -p:PublishDir=../../tmp/$2_$3 -p:PublishReadyToRun=false -p:PublishSingleFile=false -p:PublishTrimmed=false -p:UseAppHost=true
-rm tmp/$2_$3/KyoshinEewViewer
-rm tmp/$2_$3/*.dylib
 chmod +x tmp/$2_$3/KyoshinEewViewer.app/Contents/MacOS/KyoshinEewViewer
-cd tmp/$2_$3; zip -r ../KyoshinEewViewer_ingen_$2_$3.zip *
+cd tmp/$2_$3; zip -r ../KyoshinEewViewer_ingen_$2_$3.zip KyoshinEewViewer.app
