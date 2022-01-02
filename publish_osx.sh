ac29914dfc649a07fd0e186fd1b1137934a00f39
ingen084 <ingen188@gmail.com>
2022-01-03 02:04:49 +0900

    macでアセンブリをまとめないように

--- publish_osx.sh
+++ publish_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ~/.dotnet/dotnet restore -r $2 src/KyoshinEewViewer/KyoshinEewViewer.csproj
-~/.dotnet/dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=$2  -p:Configuration=Release src/KyoshinEewViewer/KyoshinEewViewer.csproj -p:PublishDir=../../tmp/$2_$3 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=true -p:UseAppHost=true
+~/.dotnet/dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=$2  -p:Configuration=Release src/KyoshinEewViewer/KyoshinEewViewer.csproj -p:PublishDir=../../tmp/$2_$3 -p:PublishReadyToRun=false -p:PublishSingleFile=false -p:PublishTrimmed=false -p:UseAppHost=true
 rm tmp/$2_$3/KyoshinEewViewer
 rm tmp/$2_$3/*.dylib
 chmod +x tmp/$2_$3/KyoshinEewViewer.app/Contents/MacOS/KyoshinEewViewer
