be6b47415d802ede30f1337bfa095967cb8a41bd
ingen084 <ingen188@gmail.com>
2022-01-03 15:53:57 +0900

    ビルド前にクリーンするように

--- publish_custom.bat
+++ publish_custom.bat
@@ -1,3 +1,3 @@
+dotnet clean src/KyoshinEewViewer/KyoshinEewViewer.csproj -r %2 -c release
 dotnet publish src/KyoshinEewViewer/KyoshinEewViewer.csproj -r %2 -c release -o tmp/%2_%3 -p:PublishReadyToRun=false -p:PublishSingleFile=true -p:PublishTrimmed=%4 --self-contained %4
-
 powershell -c "Compress-Archive -Path tmp/%2_%3/* -DestinationPath tmp/KyoshinEewViewer_ingen_%2_%3.zip"
