ef924b2bbef3d8af4d8f00a9b3f1fb6919a1b5fe
ingen084 <ingen188@gmail.com>
2022-01-22 07:21:20 +0900

    マークダウンのライブラリをサブモジュールに

---
+++ src/KyoshinEewViewer/Controls/HyperlinkTextBlock.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using System;
+
+namespace KyoshinEewViewer.Controls;
+
+public class HyperlinkTextBlock : TextBlock
+{
+	public static readonly StyledProperty<Uri?> UriProperty =
+	AvaloniaProperty.Register<HyperlinkTextBlock, Uri?>(nameof(Uri));
+
+	public Uri? Uri
+	{
+		get => GetValue(UriProperty);
+		set => SetValue(UriProperty, value);
+	}
+
+	public HyperlinkTextBlock() : base()
+	{
+		Cursor = new Cursor(StandardCursorType.Hand);
+		TextDecorations = new TextDecorationCollection() 
+		{
+			new TextDecoration() { Location = TextDecorationLocation.Underline },
+		};
+		Tapped += (s, e) =>
+		{
+			if (Uri == null)
+				return;
+			UrlOpener.OpenUrl(Uri.ToString());
+		};
+	}
+}
