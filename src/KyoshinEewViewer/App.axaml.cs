84b64bab3e265557e826b3e8e4d60139c4bd0ea6
ingen084 <ingen188@gmail.com>
2022-02-17 03:42:29 +0900

    テーマに合わせてキャプションバーの色を変更するように

--- src/KyoshinEewViewer/App.axaml.cs
+++ src/KyoshinEewViewer/App.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
@@ -14,11 +15,42 @@ using Splat;
 using System;
 using System.Diagnostics;
 using System.Reactive.Linq;
+using System.Runtime.InteropServices;
 
 namespace KyoshinEewViewer;
 
 public class App : Application
 {
+	[DllImport("dwmapi.dll", PreserveSig = true)]
+	private static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attr, ref int attrValue, int attrSize);
+	private enum DWMWINDOWATTRIBUTE
+	{
+		DWMWA_NCRENDERING_ENABLED,
+		DWMWA_NCRENDERING_POLICY,
+		DWMWA_TRANSITIONS_FORCEDISABLED,
+		DWMWA_ALLOW_NCPAINT,
+		DWMWA_CAPTION_BUTTON_BOUNDS,
+		DWMWA_NONCLIENT_RTL_LAYOUT,
+		DWMWA_FORCE_ICONIC_REPRESENTATION,
+		DWMWA_FLIP3D_POLICY,
+		DWMWA_EXTENDED_FRAME_BOUNDS,
+		DWMWA_HAS_ICONIC_BITMAP,
+		DWMWA_DISALLOW_PEEK,
+		DWMWA_EXCLUDED_FROM_PEEK,
+		DWMWA_CLOAK,
+		DWMWA_CLOAKED,
+		DWMWA_FREEZE_REPRESENTATION,
+		DWMWA_PASSIVE_UPDATE_MODE,
+		DWMWA_USE_HOSTBACKDROPBRUSH,
+		DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+		DWMWA_WINDOW_CORNER_PREFERENCE = 33,
+		DWMWA_BORDER_COLOR,
+		DWMWA_CAPTION_COLOR,
+		DWMWA_TEXT_COLOR,
+		DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
+		DWMWA_LAST
+	};
+
 	public static ThemeSelector? Selector { get; private set; }
 	public static MainWindow? MainWindow { get; private set; }
 
@@ -34,7 +66,9 @@ public class App : Application
 			Selector.EnableThemes(this);
 			ConfigurationService.Load();
 
+#if DEBUG
 			Trace.Listeners.Add(new LoggingTraceListener());
+#endif
 
 			Selector.ApplyTheme(ConfigurationService.Current.Theme.WindowThemeName, ConfigurationService.Current.Theme.IntensityThemeName);
 
@@ -48,8 +82,33 @@ public class App : Application
 					ConfigurationService.Current.Theme.IntensityThemeName = x?.Name ?? "Standard";
 					FixedObjectRenderer.UpdateIntensityPaintCache(desktop.MainWindow);
 				});
-			Selector.WhenAnyValue(x => x.SelectedWindowTheme).Where(x => x != null)
-				.Subscribe(x => ConfigurationService.Current.Theme.WindowThemeName = x?.Name ?? "Light");
+			Selector.WhenAnyValue(x => x.SelectedWindowTheme).Where(x => x != null).Subscribe(x =>
+			{
+				ConfigurationService.Current.Theme.WindowThemeName = x?.Name ?? "Light";
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && desktop.MainWindow.PlatformImpl is not null)
+				{
+					Avalonia.Media.Color FindColorResource(string name)
+						=> (Avalonia.Media.Color)(desktop.MainWindow.FindResource(name) ?? throw new Exception($"リソース {name} が見つかりませんでした"));
+					bool FindBoolResource(string name)
+						=> (bool)(desktop.MainWindow.FindResource(name) ?? throw new Exception($"リソース {name} が見つかりませんでした"));
+
+					var isDarkTheme = FindBoolResource("IsDarkTheme");
+					var USE_DARK_MODE = isDarkTheme ? 1 : 0;
+					DwmSetWindowAttribute(
+						desktop.MainWindow.PlatformImpl.Handle.Handle,
+						DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE,
+						ref USE_DARK_MODE,
+						Marshal.SizeOf(USE_DARK_MODE));
+
+					var color = FindColorResource("DockTitleBackgroundColor");
+					var colord = color.R | color.G << 8 | color.B << 16;
+					DwmSetWindowAttribute(
+						desktop.MainWindow.PlatformImpl.Handle.Handle,
+						DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR,
+						ref colord,
+						Marshal.SizeOf(colord));
+				}
+			});
 
 			desktop.Exit += (s, e) =>
 			{
--- src/Sandboxes/CustomRenderItemTest/App.axaml.cs
+++ src/Sandboxes/CustomRenderItemTest/App.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Platform;
@@ -11,11 +12,42 @@ using ReactiveUI;
 using System;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Runtime.InteropServices;
 
 namespace CustomRenderItemTest;
 
 public class App : Application
 {
+	[DllImport("dwmapi.dll", PreserveSig = true)]
+	private static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attr, ref int attrValue, int attrSize);
+	private enum DWMWINDOWATTRIBUTE
+	{
+		DWMWA_NCRENDERING_ENABLED,
+		DWMWA_NCRENDERING_POLICY,
+		DWMWA_TRANSITIONS_FORCEDISABLED,
+		DWMWA_ALLOW_NCPAINT,
+		DWMWA_CAPTION_BUTTON_BOUNDS,
+		DWMWA_NONCLIENT_RTL_LAYOUT,
+		DWMWA_FORCE_ICONIC_REPRESENTATION,
+		DWMWA_FLIP3D_POLICY,
+		DWMWA_EXTENDED_FRAME_BOUNDS,
+		DWMWA_HAS_ICONIC_BITMAP,
+		DWMWA_DISALLOW_PEEK,
+		DWMWA_EXCLUDED_FROM_PEEK,
+		DWMWA_CLOAK,
+		DWMWA_CLOAKED,
+		DWMWA_FREEZE_REPRESENTATION,
+		DWMWA_PASSIVE_UPDATE_MODE,
+		DWMWA_USE_HOSTBACKDROPBRUSH,
+		DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+		DWMWA_WINDOW_CORNER_PREFERENCE = 33,
+		DWMWA_BORDER_COLOR,
+		DWMWA_CAPTION_COLOR,
+		DWMWA_TEXT_COLOR,
+		DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
+		DWMWA_LAST
+	};
+
 	public static ThemeSelector? Selector;
 
 	public override void Initialize() => AvaloniaXamlLoader.Load(this);
@@ -32,6 +64,32 @@ public class App : Application
 			};
 			Selector.WhenAnyValue(x => x.SelectedIntensityTheme).Where(x => x != null)
 				.Subscribe(x => FixedObjectRenderer.UpdateIntensityPaintCache(desktop.MainWindow));
+			Selector.WhenAnyValue(x => x.SelectedWindowTheme).Where(x => x != null).Subscribe(x =>
+				{
+					if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && desktop.MainWindow.PlatformImpl is not null)
+					{
+						Avalonia.Media.Color FindColorResource(string name)
+							=> (Avalonia.Media.Color)(desktop.MainWindow.FindResource(name) ?? throw new Exception($"マップリソース {name} が見つかりませんでした"));
+						bool FindBoolResource(string name)
+							=> (bool)(desktop.MainWindow.FindResource(name) ?? throw new Exception($"リソース {name} が見つかりませんでした"));
+
+						var isDarkTheme = FindBoolResource("IsDarkTheme");
+						var USE_DARK_MODE = isDarkTheme ? 1 : 0;
+						DwmSetWindowAttribute(
+							desktop.MainWindow.PlatformImpl.Handle.Handle,
+							DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE,
+							ref USE_DARK_MODE,
+							Marshal.SizeOf(USE_DARK_MODE));
+
+						var color = FindColorResource("DockTitleBackgroundColor");
+						var colord = color.R | color.G << 8 | color.B << 16;
+						DwmSetWindowAttribute(
+							desktop.MainWindow.PlatformImpl.Handle.Handle,
+							DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR,
+							ref colord,
+							Marshal.SizeOf(colord));
+					}
+				});
 			desktop.Exit += (s, e) => MessageBus.Current.SendMessage(new ApplicationClosing());
 		}
 		base.OnFrameworkInitializationCompleted();
