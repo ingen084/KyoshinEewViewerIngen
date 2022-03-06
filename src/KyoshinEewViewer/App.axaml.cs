4c04eed64ebfe71afcb49753b59523f37c950293
ingen084 <ingen188@gmail.com>
2022-03-06 21:48:46 +0900

    サブウィンドウのウィンドウカラーも設定できるように

--- src/KyoshinEewViewer/App.axaml.cs
+++ src/KyoshinEewViewer/App.axaml.cs
@@ -16,41 +16,12 @@ using System;
 using System.Diagnostics;
 using System.Reactive.Linq;
 using System.Runtime.InteropServices;
+using static KyoshinEewViewer.NativeMethods;
 
 namespace KyoshinEewViewer;
 
 public class App : Application
 {
-	[DllImport("dwmapi.dll", PreserveSig = true)]
-	private static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE attr, ref int attrValue, int attrSize);
-	private enum DWMWINDOWATTRIBUTE
-	{
-		DWMWA_NCRENDERING_ENABLED,
-		DWMWA_NCRENDERING_POLICY,
-		DWMWA_TRANSITIONS_FORCEDISABLED,
-		DWMWA_ALLOW_NCPAINT,
-		DWMWA_CAPTION_BUTTON_BOUNDS,
-		DWMWA_NONCLIENT_RTL_LAYOUT,
-		DWMWA_FORCE_ICONIC_REPRESENTATION,
-		DWMWA_FLIP3D_POLICY,
-		DWMWA_EXTENDED_FRAME_BOUNDS,
-		DWMWA_HAS_ICONIC_BITMAP,
-		DWMWA_DISALLOW_PEEK,
-		DWMWA_EXCLUDED_FROM_PEEK,
-		DWMWA_CLOAK,
-		DWMWA_CLOAKED,
-		DWMWA_FREEZE_REPRESENTATION,
-		DWMWA_PASSIVE_UPDATE_MODE,
-		DWMWA_USE_HOSTBACKDROPBRUSH,
-		DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
-		DWMWA_WINDOW_CORNER_PREFERENCE = 33,
-		DWMWA_BORDER_COLOR,
-		DWMWA_CAPTION_COLOR,
-		DWMWA_TEXT_COLOR,
-		DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
-		DWMWA_LAST
-	};
-
 	public static ThemeSelector? Selector { get; private set; }
 	public static MainWindow? MainWindow { get; private set; }
 
@@ -107,14 +78,6 @@ public class App : Application
 						DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR,
 						ref colord,
 						Marshal.SizeOf(colord));
-
-					//var color2 = FindColorResource("SubForegroundColor");
-					//var colord2 = color2.R | color2.G << 8 | color2.B << 16;
-					//DwmSetWindowAttribute(
-					//	desktop.MainWindow.PlatformImpl.Handle.Handle,
-					//	DWMWINDOWATTRIBUTE.DWMWA_BORDER_COLOR,
-					//	ref colord2,
-					//	Marshal.SizeOf(colord2));
 				}
 			});
 
--- src/KyoshinEewViewer/Services/SubWindowsService.cs
+++ src/KyoshinEewViewer/Services/SubWindowsService.cs
@@ -1,5 +1,11 @@
-﻿using KyoshinEewViewer.ViewModels;
+using Avalonia.Controls;
+using KyoshinEewViewer.ViewModels;
 using KyoshinEewViewer.Views;
+using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using static KyoshinEewViewer.NativeMethods;
 
 namespace KyoshinEewViewer.Services;
 
@@ -10,28 +16,75 @@ public class SubWindowsService
 	public SettingWindow? SettingWindow { get; private set; }
 	public UpdateWindow? UpdateWindow { get; private set; }
 
+	private void ApplyTheme(Window window)
+	{
+		if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || window.PlatformImpl is null)
+			return;
+
+		Avalonia.Media.Color FindColorResource(string name)
+			=> (Avalonia.Media.Color)(window.FindResource(name) ?? throw new Exception($"リソース {name} が見つかりませんでした"));
+		bool FindBoolResource(string name)
+			=> (bool)(window.FindResource(name) ?? throw new Exception($"リソース {name} が見つかりませんでした"));
+
+		var isDarkTheme = FindBoolResource("IsDarkTheme");
+		var USE_DARK_MODE = isDarkTheme ? 1 : 0;
+		DwmSetWindowAttribute(
+			window.PlatformImpl.Handle.Handle,
+			DWMWINDOWATTRIBUTE.DWMWA_USE_IMMERSIVE_DARK_MODE,
+			ref USE_DARK_MODE,
+			Marshal.SizeOf(USE_DARK_MODE));
+
+		var color = FindColorResource("TitleBackgroundColor");
+		var colord = color.R | color.G << 8 | color.B << 16;
+		DwmSetWindowAttribute(
+			window.PlatformImpl.Handle.Handle,
+			DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR,
+			ref colord,
+			Marshal.SizeOf(colord));
+	}
+	private IDisposable Subscribe(Window window)
+		=> App.Selector.WhenAnyValue(x => x.SelectedWindowTheme).Where(x => x != null).Subscribe(x => ApplyTheme(window));
+
 	public void ShowSettingWindow()
 	{
 		if (SettingWindow == null)
 		{
-			SettingWindow = new SettingWindow
+			SettingWindow = new()
 			{
 				DataContext = new SettingWindowViewModel()
 			};
-			SettingWindow.Closed += (s, e) => SettingWindow = null;
+			var d = Subscribe(SettingWindow);
+			ApplyTheme(SettingWindow);
+			SettingWindow.Closed += (s, e) => 
+			{
+				d.Dispose();
+				SettingWindow = null;
+			};
 		}
-		SettingWindow.Show(App.MainWindow);
+		if (App.MainWindow != null)
+			SettingWindow.Show(App.MainWindow);
+		else
+			SettingWindow.Show();
 	}
 	public void ShowUpdateWindow()
 	{
 		if (UpdateWindow == null)
 		{
-			UpdateWindow = new UpdateWindow
+			UpdateWindow = new()
 			{
 				DataContext = new UpdateWindowViewModel()
 			};
-			UpdateWindow.Closed += (s, e) => UpdateWindow = null;
+			var d = Subscribe(UpdateWindow);
+			ApplyTheme(UpdateWindow);
+			UpdateWindow.Closed += (s, e) => 
+			{
+				d.Dispose();
+				UpdateWindow = null;
+			};
 		}
-		UpdateWindow.Show(App.MainWindow);
+		if (App.MainWindow != null)
+			UpdateWindow.Show(App.MainWindow);
+		else
+			UpdateWindow.Show();
 	}
 }
