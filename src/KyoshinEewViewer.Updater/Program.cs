12c9853bc0258ad247a21b8b17aaa6d61bd01b3c
ingen084 <ingen188@gmail.com>
2022-03-18 03:05:08 +0900

    run-as認識機能を削除

--- src/KyoshinEewViewer.Updater/Program.cs
+++ src/KyoshinEewViewer.Updater/Program.cs
@@ -1,9 +1,4 @@
 using Avalonia;
-using System;
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading;
 
 namespace KyoshinEewViewer.Updater;
 
@@ -14,20 +9,6 @@ static class Program
 	{
 		if (args.Length >= 1)
 			OverrideKevPath = args[0];
-		// 2つ目の引数がRunAsであれば管理者権限で再起動を試みる
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && args.Length == 2 && args[1] == "run-as")
-		{
-			var proc = new ProcessStartInfo()
-			{
-				WorkingDirectory = Environment.CurrentDirectory,
-				FileName = Assembly.GetExecutingAssembly().Location,
-				Verb = "RunAs",
-			};
-			proc.ArgumentList.Add(args[0]);
-			Process.Start(proc);
-			Thread.Sleep(2000);
-			return;
-		}
 		BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 	}
 
