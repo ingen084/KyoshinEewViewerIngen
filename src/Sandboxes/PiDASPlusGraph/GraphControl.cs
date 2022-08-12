ade05781733d7cd61770293b3959fed8f800ced9
ingen084 <ingen188@gmail.com>
2022-08-12 17:22:27 +0900

    PiDASPlusGraphを追加

---
+++ src/Sandboxes/PiDASPlusGraph/GraphControl.cs
@@ -0,0 +1,232 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using Avalonia.Threading;
+using KyoshinEewViewer.Core;
+using KyoshinEewViewer.CustomControl;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PiDASPlusGraph;
+public class GraphControl : Avalonia.Controls.Control, ICustomDrawOperation
+{
+	private float minValue = -1;
+	public static readonly DirectProperty<GraphControl, float> MinValueProperty =
+		AvaloniaProperty.RegisterDirect<GraphControl, float>(
+			nameof(MinValue),
+			o => o.MinValue,
+			(o, v) => o.MinValue = v
+		);
+	public float MinValue
+	{
+		get => minValue;
+		set {
+			if (minValue == value)
+				return;
+			minValue = value;
+			Dispatcher.UIThread.InvokeAsync(() => InvalidateVisual(), DispatcherPriority.Background).ConfigureAwait(false);
+		}
+	}
+
+	private float maxValue = 1;
+	public static readonly DirectProperty<GraphControl, float> MaxValueProperty =
+		AvaloniaProperty.RegisterDirect<GraphControl, float>(
+			nameof(MaxValue),
+			o => o.MaxValue,
+			(o, v) => o.MaxValue = v
+		);
+	public float MaxValue
+	{
+		get => maxValue;
+		set {
+			if (maxValue == value)
+				return;
+			maxValue = value;
+			Dispatcher.UIThread.InvokeAsync(() => InvalidateVisual(), DispatcherPriority.Background).ConfigureAwait(false);
+		}
+	}
+
+	private bool isAutoRange = false;
+	public static readonly DirectProperty<GraphControl, bool> IsAutoRangeProperty =
+		AvaloniaProperty.RegisterDirect<GraphControl, bool>(
+			nameof(IsAutoRange),
+			o => o.IsAutoRange,
+			(o, v) => o.IsAutoRange = v
+		);
+	public bool IsAutoRange
+	{
+		get => isAutoRange;
+		set {
+			if (isAutoRange == value)
+				return;
+			isAutoRange = value;
+			Dispatcher.UIThread.InvokeAsync(() => InvalidateVisual(), DispatcherPriority.Background).ConfigureAwait(false);
+		}
+	}
+
+	private bool isIntensityGrid = false;
+	public static readonly DirectProperty<GraphControl, bool> IsIntensityGridProperty =
+		AvaloniaProperty.RegisterDirect<GraphControl, bool>(
+			nameof(IsIntensityGrid),
+			o => o.IsIntensityGrid,
+			(o, v) => o.IsIntensityGrid = v
+		);
+	public bool IsIntensityGrid
+	{
+		get => isIntensityGrid;
+		set {
+			if (isIntensityGrid == value)
+				return;
+			isIntensityGrid = value;
+			Dispatcher.UIThread.InvokeAsync(() => InvalidateVisual(), DispatcherPriority.Background).ConfigureAwait(false);
+		}
+	}
+
+	private Dictionary<SKColor, float[]>? data;
+	public static readonly DirectProperty<GraphControl, Dictionary<SKColor, float[]>?> DataProperty =
+		AvaloniaProperty.RegisterDirect<GraphControl, Dictionary<SKColor, float[]>?>(
+			nameof(Data),
+			o => o.Data,
+			(o, v) => o.Data = v
+		);
+	public Dictionary<SKColor, float[]>? Data
+	{
+		get => data;
+		set {
+			if (data == value)
+				return;
+			data = value;
+			Dispatcher.UIThread.InvokeAsync(() => InvalidateVisual(), DispatcherPriority.Background).ConfigureAwait(false);
+		}
+	}
+
+	public GraphControl()
+	{
+		ClipToBounds = true;
+	}
+
+	public override void Render(DrawingContext context) => context.Custom(this);
+
+	public bool Equals(ICustomDrawOperation? other) => false;
+	public bool HitTest(Point p) => true;
+	public void Dispose() => GC.SuppressFinalize(this);
+
+	SKPaint EdgePaint { get; set; } = new SKPaint
+	{
+		Style = SKPaintStyle.Fill,
+		StrokeWidth = 2,
+		IsAntialias = true,
+		Typeface = FixedObjectRenderer.MainTypeface,
+		TextSize = 14,
+	};
+	SKPaint GridPaint { get; set; } = new SKPaint
+	{
+		Style = SKPaintStyle.Fill,
+		StrokeWidth = 1,
+		IsAntialias = true,
+	};
+	SKPaint BodyPaint { get; set; } = new SKPaint
+	{
+		Style = SKPaintStyle.Fill,
+		StrokeWidth = 1,
+		IsAntialias = true,
+	};
+
+	public void UpdateResources()
+	{
+		SKColor FindColorResource(string name)
+			=> ((Color)(this.FindResource(name) ?? throw new Exception($"マップリソース {name} が見つかりませんでした"))).ToSKColor();
+
+		GridPaint.Color = EdgePaint.Color = FindColorResource("SubForegroundColor");
+	}
+
+
+	public void Render(IDrawingContextImpl context)
+	{
+		var canvas = context.TryGetSkiaDrawingContext()?.SkCanvas;
+		if (canvas is null)
+			return;
+
+		float VerticalHeaderSize = IsIntensityGrid ? 15 : 30;
+
+		canvas.Save();
+		try
+		{
+			// スケール計算
+			var graphTop = MaxValue;
+			var graphBottom = MinValue;
+
+			if (IsAutoRange && Data != null)
+			{
+				var t = Data.Select(d => d.Value.Max()).Max();
+				graphTop = Math.Max(MaxValue, t + 10 - (t % 10));
+				var b = Data.Select(d => d.Value.Min()).Min();
+				graphBottom = Math.Min(MinValue, b - (b % 10));
+
+				var tbm = Math.Max(Math.Abs(graphTop), Math.Abs(graphBottom));
+				graphTop = tbm;
+				graphBottom = -tbm;
+			}
+
+			// 左ヘッダ部分
+			canvas.DrawLine(new(VerticalHeaderSize, 0), new(VerticalHeaderSize, (float)Bounds.Height), EdgePaint);
+			canvas.DrawText(graphTop.ToString(), new SKPoint(VerticalHeaderSize * .9f - EdgePaint.MeasureText(graphTop.ToString()), EdgePaint.TextSize), EdgePaint);
+			canvas.DrawText(graphBottom.ToString(), new SKPoint(VerticalHeaderSize * .9f - EdgePaint.MeasureText(graphBottom.ToString()), (float)Bounds.Height), EdgePaint);
+			// 原点
+			if (graphTop >= 0 && graphBottom <= 0)
+			{
+				var h = graphTop / (graphTop - graphBottom) * Bounds.Height;
+				canvas.DrawLine(new(VerticalHeaderSize, (float)h), new((float)Bounds.Width, (float)h), EdgePaint);
+				if (graphTop != 0 && graphBottom != 0)
+					canvas.DrawText("0".ToString(), new SKPoint(VerticalHeaderSize * .9f - EdgePaint.MeasureText("0".ToString()), (float)(h + EdgePaint.TextSize / 2)), EdgePaint);
+			}
+
+			if (IsIntensityGrid)
+				foreach (var v in new[] { 0.5, 1.5, 2.5, 3.5, 4.5, 5.0, 5.5, 6.0, 6.5 })
+				{
+					var h = v / (graphTop - graphBottom) * Bounds.Height;
+					canvas.DrawLine(new(VerticalHeaderSize, (float)h), new((float)Bounds.Width, (float)h), GridPaint);
+				}
+			//else
+			//{
+			//	// グリッド描画
+			//	var gridSpan = (graphTop - graphBottom) / 10;
+			//	for (var i = 0; i < 10; i++)
+			//	{
+			//		var c = graphBottom + gridSpan * i;
+			//		var h = c / (graphTop - graphBottom) * Bounds.Height;
+			//		if (c != 0)
+			//			canvas.DrawLine(new(VerticalHeaderSize, (float)h), new((float)Bounds.Width, (float)h), GridPaint);
+			//	}
+			//}
+
+			// グラフ本体描画
+			if (Data == null || Data.Count <= 0)
+				return;
+			var len = Data.First().Value.Length;
+			// 1点の横幅
+			var step = (Bounds.Width - VerticalHeaderSize) / len;
+			foreach (var item in Data)
+			{
+				BodyPaint.Color = item.Key;
+				var befPoint = new SKPoint();
+				for (var i = 0; i < item.Value.Length && i < len; i++)
+				{
+					var p = new SKPoint((float)(VerticalHeaderSize + i * step), (float)((graphTop - item.Value[i]) / (graphTop - graphBottom) * Bounds.Height));
+					if (i > 0)
+						canvas.DrawLine(befPoint, p, BodyPaint);
+					befPoint = p;
+				}
+			}
+		}
+		finally
+		{
+			canvas.Restore();
+		}
+	}
+}
---
+++ src/Sandboxes/PiDASPlusGraph/MainWindow.axaml.cs
@@ -0,0 +1,151 @@
+using Avalonia.Controls;
+using Avalonia.Threading;
+using KyoshinMonitorLib;
+using SkiaSharp;
+using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiDASPlusGraph
+{
+	public partial class MainWindow : Window
+	{
+		private SerialPort Serial { get; } = new()
+		{
+			BaudRate = 115200,
+			NewLine = "\r\n",
+			ReadTimeout = 10000,
+			DtrEnable = true,
+			RtsEnable = true,
+		};
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			connectButton.Tapped += (s, e) =>
+			{
+				if (Serial.IsOpen)
+				{
+					Serial.Close();
+					return;
+				}
+				if (selectDeviceBox.SelectedItem is not string port)
+					return;
+				Serial.PortName = port;
+				Serial.Open();
+				connectButton.Content = "切断";
+				Task.Run(SerialReceiveTask);
+			};
+
+			devicesUpdateButton.Tapped += (s, e) => UpdateSerialDevices();
+			UpdateSerialDevices();
+
+			intensityGraph.UpdateResources();
+			intensityGraph.Data = new() { { SKColors.Red, IntensityHistory } };
+			accGraph.UpdateResources();
+			accGraph.Data = new() {
+				{ SKColors.Tomato.WithAlpha(200), XHistory },
+				{ SKColors.DarkCyan.WithAlpha(200), YHistory },
+				{ SKColors.Orange.WithAlpha(200), ZHistory },
+			};
+		}
+
+		private float[] IntensityHistory { get; } = new float[100];
+		private float[] XHistory { get; } = new float[1000];
+		private float[] YHistory { get; } = new float[1000];
+		private float[] ZHistory { get; } = new float[1000];
+
+		private void SerialReceiveTask()
+		{
+			try
+			{
+				while (Serial.IsOpen)
+				{
+					var line = Serial.ReadLine();
+					// NMEAセンテンスであることを確認
+					if (line.Length < 1 || line[0] != '$')
+						return;
+
+					// チェックサム確認
+					var csIndex = line.IndexOf('*');
+					string[] parts;
+					if (csIndex != -1)
+					{
+						parts = line[1..csIndex].Split(',');
+						// チェックサムを取得
+						var chS = line[(csIndex + 1)..];
+						byte checkSum = 0;
+						foreach (var b in line[1..csIndex])
+							checkSum ^= (byte)b;
+						if (chS != checkSum.ToString("X2"))
+						{
+							Debug.WriteLine("cserr: " + line[1..csIndex]);
+							continue;
+						}
+					}
+					// チェックサムなし
+					else
+						parts = line[1..].Split(',');
+
+					//Debug.WriteLine(line);
+
+					switch (parts[0])
+					{
+						case "XSOFF" when parts.Length >= 2:
+							Dispatcher.UIThread.InvokeAsync(() => adjustPanel.IsVisible = parts[1] == "1");
+							if (parts[1] == "0")
+							{
+								Array.Clear(IntensityHistory);
+								Array.Clear(XHistory);
+								Array.Clear(YHistory);
+								Array.Clear(ZHistory);
+							}
+							break;
+						case "XSINT" when parts.Length >= 3:
+							Buffer.BlockCopy(IntensityHistory, 0, IntensityHistory, sizeof(float), (IntensityHistory.Length - 1) * sizeof(float));
+							IntensityHistory[0] = Math.Clamp(float.TryParse(parts[2], out var ri) ? ri : IntensityHistory[1], intensityGraph.MinValue, intensityGraph.MaxValue);
+							Dispatcher.UIThread.InvokeAsync(() =>
+							{
+								timeText.Text = DateTime.Now.ToString("MM/dd HH:mm:ss");
+								rawIntText.Text = parts[2];
+								intensity.Intensity = ((double)ri).ToJmaIntensity();
+								intensityGraph.InvalidateVisual();
+							});
+							break;
+						case "XSACC" when parts.Length >= 4:
+							Buffer.BlockCopy(XHistory, 0, XHistory, sizeof(float), (XHistory.Length - 1) * sizeof(float));
+							XHistory[0] = float.TryParse(parts[1], out var x) ? x : XHistory[1];
+							Buffer.BlockCopy(YHistory, 0, YHistory, sizeof(float), (YHistory.Length - 1) * sizeof(float));
+							YHistory[0] = float.TryParse(parts[2], out var y) ? y : YHistory[1];
+							Buffer.BlockCopy(ZHistory, 0, ZHistory, sizeof(float), (ZHistory.Length - 1) * sizeof(float));
+							ZHistory[0] = float.TryParse(parts[3], out var z) ? z : ZHistory[1];
+							Dispatcher.UIThread.InvokeAsync(() =>
+							{
+								accXText.Text = parts[1] + "gal";
+								accYText.Text = parts[2] + "gal";
+								accZText.Text = parts[3] + "gal";
+								accGraph.InvalidateVisual();
+							});
+							break;
+					}
+				}
+			}
+			catch (Exception e) when (e is TimeoutException || e is OperationCanceledException)
+			{
+				Serial.Close();
+				Dispatcher.UIThread.InvokeAsync(() => connectButton.Content = "接続");
+			}
+		}
+
+		private void UpdateSerialDevices()
+		{
+			var ports = SerialPort.GetPortNames();
+			selectDeviceBox.Items = ports;
+			if (selectDeviceBox.SelectedItem == null)
+				selectDeviceBox.SelectedItem = ports.FirstOrDefault();
+		}
+	}
+}
