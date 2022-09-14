2b8f53139420b38ff20fa86b3aff1ec344372b19
ingen084 <ingen188@gmail.com>
2022-09-15 04:24:24 +0900

    バージョン再び修正

--- src/KyoshinEewViewer.Core/Utils.cs
+++ src/KyoshinEewViewer.Core/Utils.cs
@@ -12,12 +12,12 @@ public static class Utils
 				return OverrideVersion;
 			var ver = Assembly.GetExecutingAssembly().GetName().Version;
 			if (ver == null)
-				return "不明";
+				return "?";
 			// 0.1.1.X は手元ビルドかリリース外のビルド
 			if (ver.Major == 0 && ver.Minor == 1 && ver.Build == 1)
 			{
 				if (ver.Revision != 0)
-					return "実験" + ver.Revision;
+					return "EXP-" + ver.Revision;
 				return "DEBUG";
 			}
 			return ver.ToString();
