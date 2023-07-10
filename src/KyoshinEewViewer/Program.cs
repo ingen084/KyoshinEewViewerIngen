288149293de17eab501e5253f83cbc5fa3f6bfdd
ingen084 <ingen188@gmail.com>
2023-07-10 23:34:53 +0900

    フレームワークのバージョンアップ

--- src/KyoshinEewViewer/Program.cs
+++ src/KyoshinEewViewer/Program.cs
@@ -34,8 +34,10 @@ internal static class Program
 		.LogToTrace()
 		.With(new Win32PlatformOptions
 		{
-			UseLowLatencyDxgiSwapChain = true,
-			// CompositionMode = new[] { Win32CompositionMode.LowLatencyDxgiSwapChain },
+			CompositionMode = new[] { 
+				Win32CompositionMode.LowLatencyDxgiSwapChain,
+				Win32CompositionMode.WinUIComposition,
+			},
 		})
 		.With(new FontManagerOptions
 		{
