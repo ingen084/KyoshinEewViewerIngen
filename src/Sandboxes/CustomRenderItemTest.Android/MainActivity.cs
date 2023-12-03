1722ba2129b8c78ba1e2f90540b4870731b41a0c
ingen084 <ingen188@gmail.com>
2023-12-03 13:44:29 +0900

    Android向けの微調整

--- src/Sandboxes/CustomRenderItemTest.Android/MainActivity.cs
+++ src/Sandboxes/CustomRenderItemTest.Android/MainActivity.cs
@@ -1,10 +1,22 @@
 using Android.App;
 using Android.Content.PM;
+using Avalonia;
 using Avalonia.Android;
+using Avalonia.ReactiveUI;
 
-namespace AvaloniaWebTest.Android;
+namespace CustomRenderItemTest.Android;
 
-[Activity(Label = "AvaloniaWebTest.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-public class MainActivity : AvaloniaMainActivity
+[Activity(Label = "CustomRenderItemTest.Android",
+	Theme = "@style/MyTheme.NoActionBar",
+	Icon = "@drawable/icon",
+	MainLauncher = true,
+	LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+public class MainActivity : AvaloniaMainActivity<App>
 {
+	protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+	{
+		return base.CustomizeAppBuilder(builder)
+			.UseSkia()
+			.UseReactiveUI();
+	}
 }
