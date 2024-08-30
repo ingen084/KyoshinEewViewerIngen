4f2e4f5ce5fc980b788f1a7f2a61d97798120736
ingen084 <ingen188@gmail.com>
2024-08-31 03:36:35 +0900

    作業中

---
+++ src/KyoshinEewViewer/Series/KyoshinMonitor/EarthquakeInformationHost.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyoshinEewViewer.Series.KyoshinMonitor;
+
+public abstract class EarthquakeInformationHost
+{
+	// MapDisplay Props
+}
