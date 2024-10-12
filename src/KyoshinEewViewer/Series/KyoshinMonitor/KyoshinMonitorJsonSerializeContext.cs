c88210a36ed09517c75a112b60e530c84980e04b
ingen084 <ingen188@gmail.com>
2024-10-13 05:47:56 +0900

    ライブラリ更新と追従

--- src/KyoshinEewViewer/Series/KyoshinMonitor/KyoshinMonitorJsonSerializeContext.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace KyoshinEewViewer.Series.KyoshinMonitor;
-
-[JsonSerializable(typeof(KyoshinMonitorLib.ApiResult.WebApi.Eew))]
-public partial class KyoshinMonitorJsonSerializeContext : JsonSerializerContext
-{
-}
