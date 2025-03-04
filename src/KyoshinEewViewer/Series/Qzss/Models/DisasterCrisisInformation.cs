2a544e83a921cd883068c0ddabb1ea8d857d9bff
ingen084 <ingen188@gmail.com>
2025-03-05 05:53:22 +0900

    実装準備中

--- src/KyoshinEewViewer/Series/Qzss/Models/DisasterCrisisInformation.cs
+++ src/KyoshinEewViewer/Series/Qzss/Models/DisasterCrisisInformation.cs
@@ -1,2 +1,4 @@
+using ReactiveUI;
+
 namespace KyoshinEewViewer.Series.Qzss.Models;
-public abstract class DisasterCrisisInformation;
+public abstract class DisasterCrisisInformation : ReactiveObject;
